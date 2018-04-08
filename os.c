ab02372369ba04fdbc54df9d6c01c03adc41c975
biswarup <bis.banerjee.bb@gmail.com>
2018-04-09 00:36:09 +0530

    added main function

--- os.c
+++ os.c
@@ -4,3 +4,33 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
+void* rdr();
+void* wrtr();
+pthread_mutex_t mr,mw;
+int shared_value=0;
+int readcount=0;
+int main()
+{ pthread_mutex_init(&mr,NULL);
+  pthread_mutex_init(&mw,NULL);
+  int r=0,w=0,i=0;
+  puts("enter the number of readers");
+  scanf("%d",&r);
+  puts("enter the number of writer");
+  scanf("%d",&w);
+  pthread_t t1[r],t2[w];
+  for(i=0;i<r;i++)
+  {
+  pthread_create(&t1[i],NULL,rdr,NULL);
+  }
+  for(i=0;i<w;i++)
+  {
+  pthread_create(&t2[i],NULL,wrtr,NULL);
+  }
+  for(i=0;i<r;i++)
+    pthread_join(t1[i],NULL);
+  for(i=0;i<w;i++)
+    pthread_join(t2[i],NULL);
+
+
+
+}
