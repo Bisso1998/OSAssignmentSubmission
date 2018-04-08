39b103aef9c27d92f64afb52b430923ef853c8ba
biswarup <bis.banerjee.bb@gmail.com>
2018-04-09 00:46:27 +0530

    error fixes

--- os.c
+++ os.c
@@ -4,60 +4,57 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
-void* rdr();
-void* wrtr();
-pthread_mutex_t mr,mw;
-int shared_value=0;
-int readcount=0;
+void* reader();
+void* writer();
+pthread_mutex_t mRead,mWrite;
+int sharedValue=0;
+int readCount=0;
 int main()
-{ pthread_mutex_init(&mr,NULL);
-  pthread_mutex_init(&mw,NULL);
+{ pthread_mutex_init(&mRead,NULL);
+  pthread_mutex_init(&mWrite,NULL);
   int r=0,w=0,i=0;
-  puts("enter the number of readers");
+  puts("Enter the number of readers");
   scanf("%d",&r);
-  puts("enter the number of writer");
+  puts("Enter the number of writer");
   scanf("%d",&w);
   pthread_t t1[r],t2[w];
   for(i=0;i<r;i++)
   {
-  pthread_create(&t1[i],NULL,rdr,NULL);
+  pthread_create(&t1[i],NULL,reader,NULL);
   }
   for(i=0;i<w;i++)
   {
-  pthread_create(&t2[i],NULL,wrtr,NULL);
+  pthread_create(&t2[i],NULL,writer,NULL);
   }
   for(i=0;i<r;i++)
     pthread_join(t1[i],NULL);
   for(i=0;i<w;i++)
     pthread_join(t2[i],NULL);
-
-
-
 }
-void* rdr()
-{ pthread_mutex_lock(&mr);
+void* reader()
+{ pthread_mutex_lock(&mRead);
   printf(" A reader is trying to read");
-  readcount++;
-  if(readcount==1)
-    pthread_mutex_lock(&mw);
-    pthread_mutex_unlock(&mr);
-    printf("The read value: %d",shared_value);
-    printf("Number of Readers:%d",readcount);
-    readcount--;
-  if(readcount==0)
-    pthread_mutex_unlock(&mw);
+  readCount++;
+  if(readCount==1)
+    pthread_mutex_lock(&mWrite);
+    pthread_mutex_unlock(&mRead);
+    printf("The read value: %d",sharedValue);
+    printf("Number of Readers:%d",readCount);
+    readCount--;
+  if(readCount==0)
+    pthread_mutex_unlock(&mWrite);
     printf("A reader left");
 }
 
 
-void* wrtr()
-{ pthread_mutex_lock(&mw);
+void* writer()
+{ pthread_mutex_lock(&mWrite);
   int x;
-  x=shared_value;
+  x=sharedValue;
   x++;
-  shared_value=x;
-  printf("Written Value is %d",shared_value);
-  printf("Number of Readers:%d",readcount);
-  pthread_mutex_unlock(&mw);
+  sharedValue=x;
+  printf("Written Value is %d",sharedValue);
+  printf("Number of Readers:%d",readCount);
+  pthread_mutex_unlock(&mWrite);
 
 }
